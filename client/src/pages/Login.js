fdea23c48091c6ce69d76db4511f25dd37abfdd2
sara teshome <tes28sar@gmail.com>
2022-11-06 22:38:37 -0800

    worked on components and pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -70,7 +70,7 @@ const Login = (props) => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn btn-block btn-info"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
