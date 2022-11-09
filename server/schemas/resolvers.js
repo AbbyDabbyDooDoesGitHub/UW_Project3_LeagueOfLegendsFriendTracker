9aec2b1318b95bc16ddb088042087712e78d5afa
sara teshome <tes28sar@gmail.com>
2022-11-09 10:06:49 -0800

    something went wrong

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -48,12 +48,13 @@ const resolvers = {
 
       return { token, user };
     },
-    addAccount: async (parent, { gameNote, gamerName }, context) => {
+    addAccount: async (parent, { gamerName, gameNote }, context) => {
       if (context.user) {
         const account = await Account.create({
+          gameName,
           gameNote,
           
-          gamerName: context.user.username,
+          author: context.user.username,
         });
 
         await User.findOneAndUpdate(
@@ -65,13 +66,13 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    addFriend: async (parent, { accountId, commentText }, context) => {
+    addFriend: async (parent, { accountId, friendName, friendNote }, context) => {
       if (context.user) {
         return Account.findOneAndUpdate(
           { _id: accountId },
           {
             $addToSet: {
-              friends: { friendNote, friendName: context.user.username },
+              friends: { friendName, friendNote: context.user.username },
             },
           },
           {
@@ -86,7 +87,7 @@ const resolvers = {
       if (context.user) {
         const account = await Account.findOneAndDelete({
           _id: accountId,
-          gamerName: context.user.username,
+          author: context.user.username,
         });
 
         await User.findOneAndUpdate(
