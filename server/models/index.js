679a5f5000f97edaa43f989bb65c6ef544a7212e
unknown <gwanjuchung@yahoo.com>
2022-11-09 19:26:25 -0800

    Beginning stripe integration

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,7 @@
 const User = require('./User');
 const Account = require('./Account');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
 
 module.exports = { User, Account };
