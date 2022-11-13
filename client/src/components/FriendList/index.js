01290137ef464cfdf304be3e4d4d756df8330784
unknown <gwanjuchung@yahoo.com>
2022-11-13 11:01:04 -0800

    Testing to delete friend from friend list

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -3,8 +3,9 @@ import { useParams } from 'react-router-dom';
 // import { QUERY_ME} from "../../utils/queries"
 import { useMutation } from '@apollo/client';
 // import { QUERY_SINGLE_ACCOUNT } from '../utils../queries';
-import { REMOVE_FRIEND} from '../../utils/mutations';
+import { REMOVE_FRIEND } from '../../utils/mutations';
 // import { QUERY_ACCOUNTS, QUERY_ME, QUERY_USER } from '../../utils/queries';
+import AuthService from '../../utils/auth';
 
 const FriendList = ({ friends = [] }) => {
 
@@ -13,16 +14,17 @@ const FriendList = ({ friends = [] }) => {
     return <h3>No Friends added Yet</h3>;
   }
 
-  async function deleteHandler( accountId, friendId) {
-
+  async function deleteHandler(friendId) {
+    const userProfile = AuthService.getProfile()
+    console.log("testing", friendId, userProfile);
     // make mutation call
-    const {data} =await  removeFriend({
-      variables: {  accountId, friendId}
+    const { data } = await removeFriend({
+      variables: { accountId: userProfile.data._id, friendId }
     })
+    console.log("data", data);
+    // window.location.reload();
 
-    window.location.reload();
 
-   
   }
   return (
     <>
@@ -38,15 +40,15 @@ const FriendList = ({ friends = [] }) => {
             <div key={friend._id} className="col-12 mb-3 pb-3">
               <div className="p-3 bg-dark text-light">
                 <h5 className="card-header">
-                   added{' '}
+                  added{' '}
                   <span style={{ fontSize: '0.825rem' }}>
                     on {friend.createdAt}
                   </span>
                 </h5>
                 <p className="card-body">{friend.friendName}<br />
-                {friend.friendNote}</p>
+                  {friend.friendNote}</p>
               </div>
-              <button  className="btn btn-light btn-block btn-squared" onClick={() => deleteHandler(friend._id)}>Delete </button>
+              <button className="btn btn-light btn-block btn-squared" onClick={() => deleteHandler(friend._id)}>Delete </button>
             </div>
           ))}
       </div>
