9d14c8cb0095f924a028f6cbd2d593e83b4e97a5
Abigail Douglas <EburgAbby@aol.com>
2022-11-14 13:01:11 -0800

    updated banner component

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -5,15 +5,19 @@ const Banner = () => {
   return (
 
     <div class="marquee-div">
-        <ul class="marquee-contents">
+
+      <ul class="marquee-contents">
+
         <li><img src="./public/img/banner-backgrounds/league-of-legends/EVELYNN.png" height="300px" alt="Banner image" /></li>
         <li><img src="./public/img/banner-backgrounds/league-of-legends/SORAKA_EZREAL_AHRI_MISS-FORTUNE_SYNDRA.jpg" height="300px" alt="Banner image" /></li>
         <li><img src="./public/img/banner-backgrounds/league-of-legends/SORAKA.jpg" height="300px" alt="Banner image" /></li>
         <li><img src="./public/img/banner-backgrounds/league-of-legends/EVELYNN.png" height="300px" alt="Banner image" /></li>
         <li><img src="./public/img/banner-backgrounds/league-of-legends/SORAKA_EZREAL_AHRI_MISS-FORTUNE_SYNDRA.jpg" height="300px" alt="Banner image" /></li>
         <li><img src="./public/img/banner-backgrounds/league-of-legends/SORAKA.jpg" height="300px" alt="Banner image" /></li>
-        <li><img src="./public/img/banner-backgrounds/league-of-legends/TEEMO_YASUO_KINDRED_LILLIA_AHRI_THRESH_CASSIOPEIA_RIVEN_VAYNE_YONE.jpg" height="300px" alt="Banner image" /></li>          
-        </ul>
+        <li><img src="./public/img/banner-backgrounds/league-of-legends/TEEMO_YASUO_KINDRED_LILLIA_AHRI_THRESH_CASSIOPEIA_RIVEN_VAYNE_YONE.jpg" height="300px" alt="Banner image" /></li>  
+                
+      </ul>
+
     </div>
 
   );
